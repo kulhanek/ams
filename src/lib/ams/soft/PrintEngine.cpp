6ee7bdc040fdc329dcf267b0ea0fc52328aa7692
Petr Kulhanek <kulhanek@chemi.muni.cz>
2017-05-14 18:33:14 +0200

    isoftrepo

--- src/lib/ams/soft/PrintEngine.cpp
+++ src/lib/ams/soft/PrintEngine.cpp
@@ -1162,13 +1162,13 @@ void CPrintEngine::ListModAvailableModules(CTemplateParams& params,bool include_
                             CSmallString full_name;
                             full_name = name + ":" + ver;
                             params.SetParam("MODULE",full_name);
-                            params.SetParam("MODURL",CFCGIParams::EncodeString(full_name));
+                            params.SetParam("MODULEURL",CFCGIParams::EncodeString(full_name));
                             params.NextRun();
                         }
                     }
                 } else {
                     params.SetParam("MODULE",name);
-                    params.SetParam("MODURL",CFCGIParams::EncodeString(name));
+                    params.SetParam("MODULEURL",CFCGIParams::EncodeString(name));
                     params.NextRun();
                 }
             }
