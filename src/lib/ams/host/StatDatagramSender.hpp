1d22c161e77f584650fff3b72329fd018a0b71da
Petr Kulhanek <kulhanek@chemi.muni.cz>
2024-01-22 23:06:00 +0100

    stat updated

---
+++ src/lib/ams/host/StatDatagramSender.hpp
@@ -0,0 +1,40 @@
+#ifndef StatDatagramSenderH
+#define StatDatagramSenderH
+// =============================================================================
+//  AMS - Advanced Module System
+// -----------------------------------------------------------------------------
+//     Copyright (C) 2024 Petr Kulhanek (kulhanek@chemi.muni.cz)
+//
+//     This library is free software; you can redistribute it and/or
+//     modify it under the terms of the GNU Lesser General Public
+//     License as published by the Free Software Foundation; either
+//     version 2.1 of the License, or (at your option) any later version.
+//
+//     This library is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//     Lesser General Public License for more details.
+//
+//     You should have received a copy of the GNU Lesser General Public
+//     License along with this library; if not, write to the Free Software
+//     Foundation, Inc., 51 Franklin Street, Fifth Floor,
+//     Boston, MA  02110-1301  USA
+// =============================================================================
+
+#include <AMSMainHeader.hpp>
+#include <SmallString.hpp>
+
+//------------------------------------------------------------------------------
+
+class CStatDatagramSender {
+public:
+    /// send datagram
+    virtual bool SendDataToServer(const CSmallString& servername,int port);
+
+    /// get datagram flags
+    virtual int GetFlags(void);
+};
+
+//------------------------------------------------------------------------------
+
+#endif
